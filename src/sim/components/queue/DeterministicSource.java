dc1120cb44bc15b2e8a7dc9fea02f9abae7a870d
eduardo sobral <easobral@gmail.com>
2015-07-24 13:57:37 -0300

    adicionado e escrita classe geradora com tempos determinístico entre chegadas

---
+++ src/sim/components/queue/DeterministicSource.java
@@ -0,0 +1,51 @@
+package sim.components.queue;
+
+import sim.components.basic.Job;
+import sim.components.basic.Node;
+import sim.components.basic.Sink;
+import sim.timer.TimedTask;
+import sim.timer.Timer;
+
+public class DeterministicSource extends Node {
+	private static final String inTime = "ARRIVAL_TIME";
+	
+	Sink sink;
+	Double time_between_arrivals;
+	
+	
+	class Arrival extends TimedTask implements Runnable{
+
+		public void run(){
+			Job job = new Job();
+			job.addDouble(inTime, Timer.timer.time());
+			sink.send(job);
+			programNextArrival();
+		}
+	}
+	
+	public DeterministicSource() {
+		time_between_arrivals=1D;
+	}
+
+	public DeterministicSource(Double time){
+		time_between_arrivals=time;
+	}	
+		
+		
+	public void start(){
+		programNextArrival();
+	}
+	
+	
+	private void programNextArrival() {
+		Arrival next = new Arrival();
+		next.time=Timer.now() + time_between_arrivals;
+		Timer.timer.addTask(next);
+	}
+	
+	@Override
+	public void connectTo(Sink sink){
+		this.sink=sink;
+	}
+
+}
