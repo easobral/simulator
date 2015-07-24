43136ed9594e22dfe4f2f841de4869abf0be4063
eduardo sobral <easobral@gmail.com>
2015-07-24 14:07:46 -0300

    adicionado metodo para definir a taxa e nao o tempo entre chegadas

--- src/sim/components/queue/DeterministicSource.java
+++ src/sim/components/queue/DeterministicSource.java
@@ -8,44 +8,45 @@ import sim.timer.Timer;
 
 public class DeterministicSource extends Node {
 	private static final String inTime = "ARRIVAL_TIME";
-	
+
 	Sink sink;
 	Double time_between_arrivals;
-	
-	
-	class Arrival extends TimedTask implements Runnable{
 
-		public void run(){
+	class Arrival extends TimedTask implements Runnable {
+
+		public void run() {
 			Job job = new Job();
 			job.addDouble(inTime, Timer.timer.time());
 			sink.send(job);
 			programNextArrival();
 		}
 	}
-	
+
 	public DeterministicSource() {
-		time_between_arrivals=1D;
+		time_between_arrivals = 1D;
 	}
 
-	public DeterministicSource(Double time){
-		time_between_arrivals=time;
-	}	
-		
-		
-	public void start(){
+	public DeterministicSource(Double time) {
+		time_between_arrivals = time;
+	}
+	
+	public void setRate(Double time){
+		time_between_arrivals = 1/time;
+	}
+
+	public void start() {
 		programNextArrival();
 	}
-	
-	
+
 	private void programNextArrival() {
 		Arrival next = new Arrival();
-		next.time=Timer.now() + time_between_arrivals;
+		next.time = Timer.now() + time_between_arrivals;
 		Timer.timer.addTask(next);
 	}
-	
+
 	@Override
-	public void connectTo(Sink sink){
-		this.sink=sink;
+	public void connectTo(Sink sink) {
+		this.sink = sink;
 	}
 
 }
