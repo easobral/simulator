4a9777c0892501f86cd47c8cc38f1d0e4c530919
eduardo sobral <easobral@gmail.com>
2015-07-26 18:15:56 -0300

    tretas

--- src/sim/components/queue/SystemStatistics.java
+++ src/sim/components/queue/SystemStatistics.java
@@ -1,5 +1,6 @@
 package sim.components.queue;
 
+import java.io.IOException;
 import java.io.OutputStream;
 
 import sim.components.basic.ArrivalListener;
@@ -22,11 +23,12 @@ public class SystemStatistics implements ArrivalListener, DepartureListener {
 	OutputStream departureLog;
 	String id;
 	
-	Integer jobs;
+	Integer jobs_in_system;
+	Integer total_jobs;
 	Double job_x_time;
 	Double time_in_use;
 	Double last_update;
-	Double total_time;
+	Double total_time_on_sistem;
 	
 
 	public SystemStatistics(Node entryNode, Node exitNode,
@@ -43,15 +45,15 @@ public class SystemStatistics implements ArrivalListener, DepartureListener {
 	
 	
 	public Double utilization(){
-		return time_in_use/Timer.now();
+		return (time_in_use + jobs_in_system>0?Timer.now()-last_update:0D)/Timer.now();
 	}
 	
 	public Double meanJobInSystem(){
-		return job_x_time/Timer.now();
+		return (job_x_time+(Timer.now()-last_update)*jobs_in_system)/Timer.now();
 	}
 	
 	public Double meanTimeInSystem(){
-		return null;		
+		return total_time_on_sistem/total_jobs;
 	}
 
 
@@ -65,8 +67,17 @@ public class SystemStatistics implements ArrivalListener, DepartureListener {
 	public void onArrival(Job job) {
 		Double time_slice = Timer.now()-last_update;
 		job.addDouble(id+ARRAIVAL_SUFIX, Timer.now());
-		job_x_time += jobs * time_slice;
-		time_in_use += time_slice;
+		job_x_time += jobs_in_system * time_slice;
+		time_in_use += jobs_in_system>0?time_slice:0;
+		String job_id= id +"_"+total_jobs;
+		jobs_in_system++;
+		total_jobs++;
+		String line = job_id +" "+Timer.now()+"\n";
+		try {
+			arrivalLog.write(line.getBytes());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-
 }
