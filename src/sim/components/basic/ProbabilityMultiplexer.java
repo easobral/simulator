910633dcc21eefba7a2990f734cbbd81399bf01d
eduardo sobral <easobral@gmail.com>
2015-07-26 21:53:56 -0300

    codigo formatado

--- src/sim/components/basic/ProbabilityMultiplexer.java
+++ src/sim/components/basic/ProbabilityMultiplexer.java
@@ -6,44 +6,42 @@ public class ProbabilityMultiplexer extends Multiplexer {
 
 	private ArrayList<Tuple> sinks;
 	private Double sum;
-	
+
 	class Tuple {
 		Double mass;
 		Sink sink;
 	}
-	
+
 	public ProbabilityMultiplexer() {
 		sinks = new ArrayList<>();
 	}
-	
+
 	@Override
-	public void connectTo( Sink sink ){
-		connectTo(sink,"1");
+	public void connectTo(Sink sink) {
+		connectTo(sink, "1");
 	}
-	
+
 	@Override
 	public void connectTo(Sink sink, String prob) {
 		Tuple t = new Tuple();
 		t.mass = Double.parseDouble(prob);
 		t.sink = sink;
-		sum+=t.mass;
+		sum += t.mass;
 		sinks.add(t);
 	}
-	
+
 	@Override
-	public void send (Job job){
+	public void send(Job job) {
 		onArrival(job);
-		Double random = Math.random()*sum;
-		for(Tuple t : sinks){
-			if(random<t.mass){
+		Double random = Math.random() * sum;
+		for (Tuple t : sinks) {
+			if (random < t.mass) {
 				onDeparture(job);
 				t.sink.send(job);
 				break;
 			}
-			random-=t.mass;
+			random -= t.mass;
 		}
 	}
-	
-	
 
 }
